17a1320d280247dd5898b6a490b98e514659b0e4
corinnabrungs <corinna.brungs@wwu.de>
2022-08-05 19:53:03 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+x = 20
+
+def print_hi(name="Coco"):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def print_dp(x, y):
+    print(f"{x}, {y}")
+    print("{}, {}".format(x, y))
+
+
+if __name__ == '__main__':
+    name = "COCO"
+    print_hi('PyCharm')
+    print_dp(10, x)
+    print_dp(10, x)
+    print_dp(10, x)
+    print_dp(10, x)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
