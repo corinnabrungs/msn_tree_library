131371da82fdde81ce9ca30e3ac6358f083ccec0
corinnabrungs <corinna.brungs@wwu.de>
2023-07-17 22:26:10 +0200

    pubchem search parent cid

--- test_pubchem_client.py
+++ test_pubchem_client.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 import pandas as pd
-from pubchem_client import pubchem_search_structure_by_name
+from pubchem_client import pubchem_search_structure_by_name, _get_pubchem_parent_cid
 
 
 class Test(TestCase):
@@ -15,3 +15,10 @@ class Test(TestCase):
         df = pubchem_search_structure_by_name(df)
 
         assert len(df) == 5
+
+    def test_get_pubchem_parent_cid(self):
+        cid = "46892186"
+        parent_cid = _get_pubchem_parent_cid(cid, True, 1, 1)
+        assert parent_cid == "11319217"
+        assert _get_pubchem_parent_cid("11319217", max_tries=1) == "11319217"  # is parent
+        assert _get_pubchem_parent_cid(None, max_tries=1) is None
