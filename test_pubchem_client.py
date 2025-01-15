deb23f0614e630a17fa83f6483328e2abbd793e8
corinnabrungs <53823597+corinnabrungs@users.noreply.github.com>
2025-01-15 14:38:57 +0100

    adding

--- test_pubchem_client.py
+++ test_pubchem_client.py
@@ -16,6 +16,24 @@ class Test(TestCase):
 
         assert len(df) == 4
 
+
+    def test_pubchem_search_structure_by_name_brackets(self):
+        def remove_brackets(name):
+            import re
+            return re.sub(r'\(.*?\)', '', name).strip()
+        df = pd.DataFrame(
+            {
+                "smiles": [None, ""],
+                "compound_name": ["Metformin (HCl)", "Dexlansoprazole (R-lansoprazole)"],
+            }
+        )
+        df["compound_name"] == df["compound_name"].apply(remove_brackets)
+        df = pubchem_search_structure_by_name(df)
+
+        assert len(df) == 2
+        #assert df.loc[0, 'smiles'] is not None
+        print(df.loc[1, "smiles"])
+
     def test_get_pubchem_parent_cid(self):
         cid = "46892186"
         parent_cid = _get_pubchem_parent_cid(cid, True, 1, 1)
